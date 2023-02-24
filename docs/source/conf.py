ba05574edf4eebdfa8c57707e25de103a315df43
apoulain22 <105729134+apoulain22@users.noreply.github.com>
2023-02-24 16:00:10 +0100

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,12 +2,12 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'BROADCAST'
+copyright = '2020, Cedric Content'
+author = 'Cedric Content and Arthur Poulain'
 
-release = '0.1'
-version = '0.1.0'
+release = '1.0'
+#version = '0.1.0'
 
 # -- General configuration
 
